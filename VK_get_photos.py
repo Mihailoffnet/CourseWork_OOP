f93a2390e5f69587432e0cacd4e338a5f6afdd76
Mihailoff <mihailoff@inbox.ru>
2023-04-22 19:26:48 +0500

    added def

--- VK_get_photos.py
+++ VK_get_photos.py
@@ -1,6 +1,7 @@
 import time
 import requests
 import json
+from datetime import datetime
 from pprint import pprint
 
 
@@ -31,8 +32,7 @@ class VkGetPhotos:
         params = self._get_params(owner_id, album_id, extended, 
         photo_sizes, count, api_version)
         
-        print(f'Отправляем запрос для получения информации о {count} фото пользователя Id={owner_id} в ВК')
-
+        print(f'Отправляем запрос для получения информации о {count} фото пользователя Id={owner_id} в VK')
         response = requests.get(URL, params)
         if response.status_code == 200:
             print(f'Информация о {count} фото пользователя Id={owner_id} успешно прочитана')
@@ -40,14 +40,19 @@ class VkGetPhotos:
             print(f'Ошибка запроса {response.status_code}\nРабота программы остановлена')
             exit
 
+        # получаем имя пользователя и создаем имя папки для загрузки
         folder_name = self._get_user_name(params)
         file_name = f'{folder_name}.json' 
 
         with open(file_name, "w") as f:
             json.dump(response.json(), f, ensure_ascii=False, indent=4)
-            print(f'информация о {count} фото успешно сохранена в файл {file_name} в формате json')
-        
-        return response, folder_name
+            print(f'информация о {count} последних фото успешно сохранена в файл {file_name} в формате json')
+
+
+        list_photos = self._get_list_photo(response)
+        print(f'Выбрали самые большие фотографии из {count} загруженных фото пользователя Id={owner_id}')
+            
+        return response, list_photos, folder_name
 
     def _get_user_name(self, params):
         URL = 'https://api.vk.com/method/users.get'
@@ -63,46 +68,45 @@ class VkGetPhotos:
         
         return response
 
+    def _get_list_photo(self, response):
+        # создаем список словарей без лишних ключей
+        list_photos = response.json().pop('response').pop('items')
 
+        # перебираем список словарей и составляем новый список
+        # содержащий только нужные данные по фото
+        list_photo = []
+        for dict in list_photos:
+            temp_dict = {}
+            temp_dict['id_photo'] = dict['id']
+            temp_dict['likes_photo'] = dict['likes']['count']
+            # дату фото для имени файла приводим в читаемый вид и формат str
+            temp_dict['date_photo'] = str(datetime.fromtimestamp(dict['date']))[:-9]
 
-
-
-            # for value in key:
-        # pprint(response['response'])
-        # print(type(response['response']))        
-        # pprint(response.get('response').get('first_name'))
-        # print(type(response.get('response')))
-
-        # for key in response['response']:
-        #     print(key)
-        # pprint(response.json())
-        # print(type(response.json()))
-        # # pprint(response.json()['first_name'])
-        # pprint(response.get('response').get('first_name'))
-        # # pprint(response.json()['first_name'])
-
-        return response
-
-
-        # # pprint(response)
-        # print(type(response))
-        # print(response)
-        # return response
+            for types in dict['sizes']:
+                if types['type'] == 's':
+                    size = types['type']
+                    url = types['url']
+                elif types['type'] == 'm':
+                    size = types['type']
+                    url = types['url']
+                elif types['type'] == 'x':
+                    size = types['type']  
+                    url = types['url']   
+                elif types['type'] == 'y':
+                    size = types['type']    
+                    url = types['url']
+                elif types['type'] == 'z':
+                    size = types['type']    
+                    url = types['url']   
+                else: 
+                    size = types['type']
+                    url = types['url']
+            temp_dict['size'] = size 
+            temp_dict['url'] = url 
+            list_photo.append(temp_dict)
         
+        return list_photo
 
-    # def get_params(self, user_id, api_version='5.131', 
-    #     extended=0, fields=''):
-
-    #     params = {
-    #     'user_ids': user_id,
-    #     'access_token': token, # токен и версия api являются обязательными параметрами во всех запросах к vk
-    #     'v': api_version,
-    #     'extended':extended,
-    #     'fields':fields
-    #     }
-    #     return params
-    
-    # def get_photo_list(user_id, )
 
 
 
--- main.py
+++ main.py
@@ -3,10 +3,15 @@
 from pprint import pprint
 from YaDiskUploader import YaUploader
 from VK_get_photos import VkGetPhotos
+from datetime import datetime
+import time
+import json
+
+
 
 # загружаю токены из файла. 
 # Первая строка - токен Яндекс диска,
-# Вторая строка - токен ВК с правами photos 
+# Вторая строка - токен ВК с правами scope = photos 
 
 with open('token.txt', 'r') as file_object:
     yadisk_token = file_object.readline().strip()
@@ -18,15 +23,71 @@ if __name__ == '__main__':
 
     # тест класса VkGetPhotos
     get_photo = VkGetPhotos(vk_token)
-    result_photos = get_photo.get_photos(owner_id='2737213', album_id='wall')
-    # pprint(result_photos[0].json())
-    # print(result_photos[1])
 
-    # тест класса YaUploader
-    target_path = result_photos[1]
-    filename = f'{target_path}.json'
-    target_path_to_file = f'{target_path}/{filename}'
-    path_to_file = filename
+    # можно дополнительно указать параметры:
+    # album_id='wall' - фотографии со стены, 'saved' - сохраненные фотографии. 
+    # по умолчанию загружаются фотографии профиля
+    # count= - количество загружаемых фотографий. По умолчанию 5
+    # api_version= - версия api. По умолчанию '5.131'
+    # значения по умолчанию можно изменить в функции get_photos
+    # в классе VkGetPhotos
+    result_photos = get_photo.get_photos(owner_id='1')
+    # print(result_photos[0].json())
+    print(result_photos[1])
+    print(result_photos[2])
+    print()
 
-    uploader = YaUploader(yadisk_token)
-    result_yadisk = uploader.upload(path_to_file, filename, target_path, target_path_to_file)
+
+    # # создаем список словарей без лишних ключей
+    # list_photos = result_photos[0].json().pop('response').pop('items')
+    # # pprint(list_photos)
+    # # print(type(list_photos))
+
+    # # перебираем список словарей и составляем новый список
+    # # содержащий только нужные данные по фото
+    # list_photo = []
+    # for dict in list_photos:
+    #     temp_dict = {}
+    #     temp_dict['id_photo'] = dict['id']
+    #     temp_dict['likes_photo'] = dict['likes']['count']
+    #     # дату фото для имени файла приводим в читаемый вид и формат str
+    #     temp_dict['date_photo'] = str(datetime.fromtimestamp(dict['date']))[:-9]
+
+    #     for types in dict['sizes']:
+    #         if types['type'] == 's':
+    #             size = types['type']
+    #         elif types['type'] == 'm':
+    #             size = types['type']
+    #         elif types['type'] == 'x':
+    #             size = types['type']     
+    #         elif types['type'] == 'y':
+    #             size = types['type']    
+    #         elif types['type'] == 'z':
+    #             size = types['type']       
+    #         else: size = types['type']
+    #     temp_dict['size'] = size 
+    #     list_photo.append(temp_dict)
+        
+    # print(list_photo)
+    # print()
+    
+
+        #         list.append()
+
+            # for size in ['w', 'z', 'y', 'x', 'm', 's']:
+            #     print(size)
+            #     if types['type'] == size:
+            #         print(f'самое большое изображение {size}')
+            #         break
+            # print(type(type))
+            # print()
+
+
+    # # тест класса YaUploader
+    # target_path = result_photos[1]
+    # filename = f'{target_path}.json'
+    # target_path_to_file = f'{target_path}/{filename}'
+    # path_to_file = filename
+
+    # uploader = YaUploader(yadisk_token)
+    # result_yadisk = uploader.upload(path_to_file, filename, target_path, target_path_to_file)
