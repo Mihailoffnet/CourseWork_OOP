dd81e61094ea5d73fe689018245cf3c9ad96a074
Mihailoff <127674824+Mihailoffnet@users.noreply.github.com>
2023-04-24 12:01:19 +0500

    Update VK_get_photos.py

--- VK_get_photos.py
+++ VK_get_photos.py
@@ -11,11 +11,12 @@ class VkGetPhotos:
         self.token = token
 
     def _get_params(self, owner_id, album_id, extended, 
-        photo_sizes, count, api_version):
+        photo_sizes, count, rev, api_version):
 
         params = {
         'owner_id': owner_id,
         'user_ids': owner_id,
+        'rev': rev,
         'album_id': album_id,
         'extended': extended,
         'photo_sizes': photo_sizes,
@@ -26,19 +27,57 @@ class VkGetPhotos:
         return params
     
     def get_photos(self, owner_id, album_id='profile', extended=1, 
-        photo_sizes=1, count=5, api_version='5.131'):
+        photo_sizes=1, count=5, rev=1, api_version='5.131'):
 
         URL = 'https://api.vk.com/method/photos.get'
         params = self._get_params(owner_id, album_id, extended, 
-        photo_sizes, count, api_version)
+        photo_sizes, count, rev, api_version)
+
+        log_line = f'{str(datetime.now())[:-4]}: Отправляем запрос для получения информации о {count} фото пользователя Id={owner_id} в VK'
+        with open('logs.txt', 'w') as file:
+            file.write(f'{log_line}\n')
+        print(log_line)
         
-        print(f'Отправляем запрос для получения информации о {count} фото пользователя Id={owner_id} в VK')
         response = requests.get(URL, params)
+        if 'error' in response.json():
+            error_code = response.json()['error']['error_code']
+            error_msg = response.json()['error']['error_msg']
+            log_line = f'{str(datetime.now())[:-4]}: Ошибка: {error_msg}. Код ошибки: {error_code}. Работа программы остановлена.'
+            with open('logs.txt', 'a') as file:
+                file.write(f'{log_line}\n')
+            print(log_line)
+            exit(0)
+            # if response.json()['error']['error_code'] == 30:
+            #     log_line = f'{str(datetime.now())[:-4]}: Приватный профиль. Загрузка фото невозможна. Работа программы остановлена'
+            #     with open('logs.txt', 'a') as file:
+            #         file.write(f'{log_line}\n')
+            #     print(log_line)
+            #     exit(0)
+            # elif response.json()['error']['error_code'] == 15:
+            #     log_line = f'{str(datetime.now())[:-4]}: Доступ запрещен, аккаунт пользовтаеля был удален. Загрузка фото невозможна. Работа программы остановлена'
+            #     with open('logs.txt', 'a') as file:
+            #         file.write(f'{log_line}\n')
+            #     print(log_line)
+            #     exit(0)
+            # else:
+                # log_line = f'{str(datetime.now())[:-4]}: Ошибка {response.json()['error']['error_msg']} код ошибки {response.json()['error']['error_code']}. Работа программы остановлена'
+                # with open('logs.txt', 'a') as file:
+                #     file.write(f'{log_line}\n')
+                # print(log_line)
+                # exit(0)
+
+
         if response.status_code == 200:
-            print(f'Информация о {count} фото пользователя Id={owner_id} успешно прочитана')
+            log_line = f'{str(datetime.now())[:-4]}: Информация о {count} фото пользователя Id={owner_id} успешно прочитана'
+            with open('logs.txt', 'a') as file:
+                file.write(f'{log_line}\n')
+            print(log_line)
         else:
-            print(f'Ошибка запроса {response.status_code}\nРабота программы остановлена')
-            exit
+            log_line = f'{str(datetime.now())[:-4]}: Ошибка запроса {response.status_code}. Работа программы остановлена'
+            with open('logs.txt', 'a') as file:
+                file.write(f'{log_line}\n')
+            print(log_line)
+            exit(0)
 
         # получаем имя пользователя и создаем имя папки для загрузки
         folder_name = self._get_user_name(params)
@@ -46,11 +85,16 @@ class VkGetPhotos:
 
         with open(file_name, "w") as f:
             json.dump(response.json(), f, ensure_ascii=False, indent=4)
-            print(f'информация о {count} последних фото успешно сохранена в файл {file_name} в формате json')
-
+        log_line = f'{str(datetime.now())[:-4]}: Информация о {count} последних фото успешно сохранена в файл {file_name} в формате json'
+        with open('logs.txt', 'a') as file:
+            file.write(f'{log_line}\n')          
+        print(log_line)
 
         list_photos = self._get_list_photo(response)
-        print(f'Выбрали самые большие фотографии из {count} загруженных фото пользователя Id={owner_id}')
+        log_line = f'{str(datetime.now())[:-4]}: Выбрали самые большие фотографии из {count} загруженных фото пользователя Id={owner_id}'
+        with open('logs.txt', 'a') as file:
+            file.write(f'{log_line}\n')
+        print(log_line)
             
         return response, list_photos, folder_name
 
@@ -58,12 +102,14 @@ class VkGetPhotos:
         URL = 'https://api.vk.com/method/users.get'
     
         response = requests.get(URL, params=params).json()
-
         for key in response['response']:
             first_name = key['first_name']
             last_name = key['last_name']
             id = key['id']
-            print(f'Получена информация о пользователе VK с id={id}\nЭто {first_name} {last_name}')
+            log_line = f'{str(datetime.now())[:-4]}: Получена информация о пользователе VK с id={id}. Это {first_name} {last_name}'
+            with open('logs.txt', 'a') as file:
+                file.write(f'{log_line}\n')
+            print(log_line)
         response = f'{id}_{first_name}_{last_name}'
         
         return response
@@ -81,7 +127,9 @@ class VkGetPhotos:
             temp_dict['likes_photo'] = dict['likes']['count']
             # дату фото для имени файла приводим в читаемый вид и формат str
             temp_dict['date_photo'] = str(datetime.fromtimestamp(dict['date']))[:-9]
-
+            
+            # print(len(dict['sizes']))
+            # print(dict['sizes'])
             for types in dict['sizes']:
                 if types['type'] == 's':
                     size = types['type']
