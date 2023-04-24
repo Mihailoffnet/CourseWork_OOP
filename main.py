5370862a5784160856a1c615a139f57417729259
Mihailoff <127674824+Mihailoffnet@users.noreply.github.com>
2023-04-24 17:11:55 +0500

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,19 @@
 from pprint import pprint
 from YaDiskUploader import YaUploader
 from VK_get_photos import VkGetPhotos
+from Logger import Logger
 from datetime import datetime
 import time
 import json
+import logging
+
+
+logging.basicConfig(level=logging.DEBUG, filename="py_log.log",filemode="a", format='%(asctime)s %(levelname)s %(message)s')
+# logging.debug("A DEBUG Message")
+# logging.info("An INFO")
+# logging.warning("A WARNING")
+# logging.error("An ERROR")
+# logging.critical("A message of CRITICAL severity")
 
 
 
@@ -32,7 +42,7 @@ if __name__ == '__main__':
     # api_version= - версия api. По умолчанию '5.131'
     # значения по умолчанию можно изменить в функции get_photos
     # в классе VkGetPhotos
-    result_photos = get_photo.get_photos(owner_id='5') # 2737213
+    result_photos = get_photo.get_photos(owner_id='2737213') # 2737213
     # print(result_photos[0].json())
     print()  
     pprint(result_photos[1])
@@ -41,10 +51,13 @@ if __name__ == '__main__':
     print()
 
     # тест класса YaUploader
-    target_path = result_photos[2]
-    filename = f'{target_path}.json'
-    target_path_to_file = f'{target_path}/{filename}'
-    path_to_file = filename
+
+  
+    # target_path = result_photos[2]
+    # filename = f'{target_path}.json'
+    # target_path_to_file = f'{target_path}/{filename}'
+    # path_to_file = filename
 
     uploader = YaUploader(yadisk_token)
-    result_yadisk = uploader.upload(path_to_file, filename, target_path, target_path_to_file)
+    result_yadisk1 = uploader.list_upload(result_photos[1], result_photos[2])
+    # result_yadisk2 = uploader.upload(path_to_file, filename, target_path, target_path_to_file)
