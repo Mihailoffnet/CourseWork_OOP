3f8da2988dc6373929d149fa5e8561cab20c4708
Mihailoff <127674824+Mihailoffnet@users.noreply.github.com>
2023-04-24 17:12:23 +0500

    Update VK_get_photos.py

--- VK_get_photos.py
+++ VK_get_photos.py
@@ -1,8 +1,10 @@
-import time
 import requests
 import json
 from datetime import datetime
 from pprint import pprint
+from Logger import Logger
+import logging
+import os
 
 
 class VkGetPhotos:
@@ -33,83 +35,45 @@ class VkGetPhotos:
         params = self._get_params(owner_id, album_id, extended, 
         photo_sizes, count, rev, api_version)
 
-        log_line = f'{str(datetime.now())[:-4]}: Отправляем запрос для получения информации о {count} фото пользователя Id={owner_id} в VK'
-        with open('logs.txt', 'w') as file:
-            file.write(f'{log_line}\n')
-        print(log_line)
+        Logger.get_logging(f'Отправляем запрос для получения информации о {count} фото пользователя Id={owner_id} в VK')
+
+        # запрашиваем имя пользователя и создаем имя папки для загрузки
+        folder_name = self._get_user_name(params)
+        file_name = f'{folder_name}.json'
         
-        response = requests.get(URL, params)
+        # запрашгиваем информацию о фото  
+        response = requests.get(URL, params, timeout=10)
+        # count_photo = len(self._get_list_photo(response))
         if 'error' in response.json():
             error_code = response.json()['error']['error_code']
             error_msg = response.json()['error']['error_msg']
-            log_line = f'{str(datetime.now())[:-4]}: Ошибка: {error_msg}. Код ошибки: {error_code}. Работа программы остановлена.'
-            with open('logs.txt', 'a') as file:
-                file.write(f'{log_line}\n')
-            print(log_line)
+            Logger.get_logging(f'Ошибка: {error_msg}. Код ошибки: {error_code}. Работа программы остановлена.')
             exit(0)
-            # if response.json()['error']['error_code'] == 30:
-            #     log_line = f'{str(datetime.now())[:-4]}: Приватный профиль. Загрузка фото невозможна. Работа программы остановлена'
-            #     with open('logs.txt', 'a') as file:
-            #         file.write(f'{log_line}\n')
-            #     print(log_line)
-            #     exit(0)
-            # elif response.json()['error']['error_code'] == 15:
-            #     log_line = f'{str(datetime.now())[:-4]}: Доступ запрещен, аккаунт пользовтаеля был удален. Загрузка фото невозможна. Работа программы остановлена'
-            #     with open('logs.txt', 'a') as file:
-            #         file.write(f'{log_line}\n')
-            #     print(log_line)
-            #     exit(0)
-            # else:
-                # log_line = f'{str(datetime.now())[:-4]}: Ошибка {response.json()['error']['error_msg']} код ошибки {response.json()['error']['error_code']}. Работа программы остановлена'
-                # with open('logs.txt', 'a') as file:
-                #     file.write(f'{log_line}\n')
-                # print(log_line)
-                # exit(0)
-
-
+        print
         if response.status_code == 200:
-            log_line = f'{str(datetime.now())[:-4]}: Информация о {count} фото пользователя Id={owner_id} успешно прочитана'
-            with open('logs.txt', 'a') as file:
-                file.write(f'{log_line}\n')
-            print(log_line)
+            Logger.get_logging(f'Информация о фото пользователя Id={owner_id} успешно прочитана.')
         else:
-            log_line = f'{str(datetime.now())[:-4]}: Ошибка запроса {response.status_code}. Работа программы остановлена'
-            with open('logs.txt', 'a') as file:
-                file.write(f'{log_line}\n')
-            print(log_line)
+            Logger.get_logging(f'Ошибка запроса {response.status_code}. Работа программы остановлена.')    
             exit(0)
-
-        # получаем имя пользователя и создаем имя папки для загрузки
-        folder_name = self._get_user_name(params)
-        file_name = f'{folder_name}.json' 
 
         with open(file_name, "w") as f:
             json.dump(response.json(), f, ensure_ascii=False, indent=4)
-        log_line = f'{str(datetime.now())[:-4]}: Информация о {count} последних фото успешно сохранена в файл {file_name} в формате json'
-        with open('logs.txt', 'a') as file:
-            file.write(f'{log_line}\n')          
-        print(log_line)
-
+        Logger.get_logging(f'Информация о последних фото успешно сохранена в файл {file_name} в формате json.') 
+        Logger.get_logging(f'Выбрали самые большие фотографии загруженных фото пользователя Id={owner_id}.')    
         list_photos = self._get_list_photo(response)
-        log_line = f'{str(datetime.now())[:-4]}: Выбрали самые большие фотографии из {count} загруженных фото пользователя Id={owner_id}'
-        with open('logs.txt', 'a') as file:
-            file.write(f'{log_line}\n')
-        print(log_line)
-            
+           
         return response, list_photos, folder_name
 
     def _get_user_name(self, params):
         URL = 'https://api.vk.com/method/users.get'
     
-        response = requests.get(URL, params=params).json()
+        response = requests.get(URL, params=params, timeout=10).json()
         for key in response['response']:
             first_name = key['first_name']
             last_name = key['last_name']
             id = key['id']
-            log_line = f'{str(datetime.now())[:-4]}: Получена информация о пользователе VK с id={id}. Это {first_name} {last_name}'
-            with open('logs.txt', 'a') as file:
-                file.write(f'{log_line}\n')
-            print(log_line)
+            Logger.get_logging(f'Получена информация о пользователе VK с id={id}. Это {first_name} {last_name}.') 
+
         response = f'{id}_{first_name}_{last_name}'
         
         return response
@@ -152,8 +116,28 @@ class VkGetPhotos:
             temp_dict['size'] = size 
             temp_dict['url'] = url 
             list_photo.append(temp_dict)
+
+        file_list = []
+        for line in list_photo:
+            temp_dict = {}
+            temp_dict['size'] = line['size']
+            name = line['likes_photo']
+            if f'{name}.jpg' in temp_dict.keys():
+                date_photo = line['date_photo']
+                temp_dict['file_name'] = f'{name}_{date_photo}.jpg'
+            else:
+                temp_dict['file_name'] = f'{name}.jpg'
+            file_list.append(temp_dict)
+            url = line['url']
+            target_file_name = temp_dict['file_name']
+            response = requests.get(url)
+            size = temp_dict['size']
+            with open(target_file_name, 'wb') as file:
+                file.write(response.content)
+            Logger.get_logging(f'Файл {target_file_name} размера {size} сохранен на диск.')
         
-        return list_photo
+        return file_list
+
 
 
 
