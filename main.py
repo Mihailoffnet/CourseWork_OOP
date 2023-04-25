7f4ad8a5f0f3d0e9a6b7ec676332cfd4de1e501b
Mihailoff <127674824+Mihailoffnet@users.noreply.github.com>
2023-04-25 17:02:35 +0500

    25.04.2023_17:00

--- main.py
+++ main.py
@@ -1,51 +1,65 @@
+# https://replit.com/@MihailoffVictor/courseworkoop#main.py
+
 from YaDiskUploader import YaUploader
 from VK_get_photos import VkGetPhotos
-import logging
-
-
-logging.basicConfig(level=logging.DEBUG, filename="py_log.log",filemode="a", format='%(asctime)s %(levelname)s %(message)s')
-# logging.debug("A DEBUG Message")
-# logging.info("An INFO")
-# logging.warning("A WARNING")
-# logging.error("An ERROR")
-# logging.critical("A message of CRITICAL severity")
-
 
 
 # загружаю токены из файла. 
 # Первая строка - токен Яндекс диска,
 # Вторая строка - токен ВК с правами scope = photos 
-
 with open('token.txt', 'r') as file_object:
     yadisk_token = file_object.readline().strip()
     vk_token = file_object.readline().strip()
 
-# тест класса VkGetPhotos
-
-if __name__ == '__main__':
-
-    # тест класса VkGetPhotos
+# функция для объединения функционала двух классов
+def safe_photo(owner_id, album_id='profile', extended=1, 
+        photo_sizes=1, count=5, rev=1, api_version='5.131'):
     get_photo = VkGetPhotos(vk_token)
-
-    # можно дополнительно указать параметры:
-    # album_id='wall': фотографии со стены, 'saved' - сохраненные фотографии. 
-    # по умолчанию загружаются фотографии профиля 'profile'
-    # count=: количество загружаемых фотографий. По умолчанию 5
-    # rev=: 1 - получить последние фото. 0 - получить более старые фото.
-    # по умолчанию 1, получить более новые фото.
-    # api_version= - версия api. По умолчанию '5.131'
-    # значения по умолчанию можно изменить в функции get_photos
-    # в классе VkGetPhotos
-    result_photos = get_photo.get_photos(owner_id='pisatelnaminimalkah', count=5) 
-    # 2737213 51030512 245760700
-    # print(result_photos[0].json())
-    # print()  
-    # pprint(result_photos[1])
-    # print()  
-    # pprint(result_photos[2])
-    # print()
-
-    # тест класса YaUploader
-
     uploader = YaUploader(yadisk_token)
-    result_yadisk1 = uploader.list_upload(result_photos[1], result_photos[2])
+    result_photos = get_photo.get_photos(owner_id, album_id, extended, 
+        photo_sizes, count, rev, api_version)
+    result_yadisk = uploader.list_upload(result_photos[1], result_photos[2])
+
+# запускаем программу. На вход id или логин пользователя VK
+if __name__ == '__main__':
+    print('''Программа для загрузки фото из VK в Яндекс диск.
+Для работы программы вам понадобится токен ЯндексДиска.
+
+В простом режиме программа загружает 5 файлов из профиля (с аватара) пользователя по заданному id или логину пользователя. 
+
+В расширенном режиме можно загружать неограниченное количество фотографий из профиля, со стены пользователя или из его альбома.''')
+    chose = input('''
+Хотите запустить программу в расширенном режиме? Ответьте "2".
+Что бы продолжить в упрощенном режиме, ответьте "1" или просто нажмите Enter.
+Ваш выбор: ''')
+    yadisk_token = input('''
+Скопируйте сюда свой OAuth-токен яндекс диска.
+OAuth-токен можно получить по адресу: https://yandex.ru/dev/disk/rest/
+Ваш токен: ''')
+    id = ''
+    while id == '':
+        print()
+        id = input('Введите id или login пользователя VK: ').lower()
+        print()
+    if chose != '2':
+        safe_photo(owner_id=id) # 2737213 51030512 245760700
+    else:
+        x = input('''
+Скачать фото со стены - ответьте "2"
+Скачать фото из профиля - ответьте "1" или просто нажмите Enter.
+Ваш выбор: ''')
+        if x == '2': a_id = 'wall'
+        else: a_id = 'profile'
+        y = ''
+        while not y.isnumeric():
+            y = input('''
+Введите количество фотографий, которые нужно скачать. 
+Если у пользователя окажется меньше фотографий, то будут скачаны все фотографии.
+Ваш выбор: ''')
+            
+              
+        count_photo = int(y)
+        safe_photo(owner_id=id, album_id=a_id, count=count_photo)
+      
+
+      
