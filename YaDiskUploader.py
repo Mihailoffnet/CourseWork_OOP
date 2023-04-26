79db0ce5865ae8321e1bf23665152fc01c235f0e
Mihailoff <127674824+Mihailoffnet@users.noreply.github.com>
2023-04-26 14:28:45 +0500

    26.04_3

--- YaDiskUploader.py
+++ YaDiskUploader.py
@@ -1,7 +1,5 @@
 import requests
-# from datetime import datetime
 import json
-from pprint import pprint
 from Logger import Logger
 import os
 
@@ -20,7 +18,7 @@ class YaUploader:
     def test_token(self):
         url = 'https://cloud-api.yandex.net/v1/disk'
         headers = self.get_headers()
-        response = requests.get(url = url, headers=headers, timeout=5)
+        response = requests.get(url=url, headers=headers, timeout=5)
         code = response.status_code
         if code != 200:
             message = response.json()['message']
@@ -31,17 +29,17 @@ class YaUploader:
         else:
             Logger.get_logging(f'Токен яндекс диска проверен.'
                                f' Доступ предоставлен.')
-  
+
     def _upload_link(self, target_path_to_file, target_path):
         upload_url = "https://cloud-api.yandex.net/v1/disk/resources/upload"
         headers = self.get_headers()
         params = {"path": target_path_to_file, "overwrite": "True"}
-        response = requests.get(upload_url, headers=headers, 
+        response = requests.get(upload_url, headers=headers,
                                 params=params, timeout=10)
         if response.status_code == 409:
             Logger.get_logging(f'Пути {target_path} На Ядиске не существует.')
             self._great_folder(target_path)
-            response = requests.get(upload_url, headers=headers, 
+            response = requests.get(upload_url, headers=headers,
                                     params=params, timeout=10)
             Logger.get_logging(f'Папка {target_path} создана.'
                                f' Начинаем загрузку файла.')
@@ -53,7 +51,8 @@ class YaUploader:
         href = self._upload_link(target_path_to_file, target_path)
         url = href.get('href')
         Logger.get_logging(f'Получена ссылка для загрузки файла.')
-        response = requests.put(url, data=open(path_to_file, 'rb'), timeout=120)
+        response = requests.put(url, data=open(
+            path_to_file, 'rb'), timeout=120)
         response.raise_for_status()
         if response.status_code == 201:
             Logger.get_logging(f'Файл {filename} успешно загружен на Яндекс'
@@ -70,8 +69,8 @@ class YaUploader:
         params = {'path': target_path}
         response = requests.put(url=url, headers=headers, params=params,
                                 timeout=10)
-        return response   
-      
+        return response
+
     # функция загрузки файлов из списка
     def list_upload(self, list, target_path):
         for photo in list:
@@ -89,4 +88,4 @@ class YaUploader:
             json.dump(dict_json, file, ensure_ascii=False, indent=4)
         Logger.get_logging(f'Файл {file_name} успешно сохранен.')
         Logger.get_logging(f'Работа программы завершена. Загружено {len(list)}'
-                           f' фото.\n')  
+                           f' фото.\n')
